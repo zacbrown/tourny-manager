911257f8380c62f03fe55b80252b8d0c4f829764
Zac Brown <zac@zacbrown.org>
2009-02-08 21:35:03 -0500

    * Create multi-page input system. * Add confirmation page, showing all options chosen. - Still needs SQL connectivity (SQLite or MySQL). - Needs some beautification.

--- src/main.rb
+++ src/main.rb
@@ -24,6 +24,11 @@ class TournyMgr < Shoes
   url '/', :index
   url '/register', :register
   url '/success', :success
+  url '/first_name', :first_name
+  url '/last_name', :last_name
+  url '/cnumber', :cnumber
+  url '/sex_choose', :sex_choose
+  url '/review', :review
 
   def submit_registration(first_name, last_name, c_number, sex)
 
@@ -33,79 +38,274 @@ class TournyMgr < Shoes
     register
   end
 
+  def review
+    background white
+    stack :margin => 10, :margin_left => 120, :margin_top => 20 do
+      banner "In Review"
+    end
+    stack :margin => 10, :margin_left => 110, :margin_top => 10 do
+      subtitle "You seem to think..."
+    end
+    stack :margin_left => 90, :width => 550, :margin => 10, :margin_top => 20 do
+      flow do
+        @conf_first = para "* your first name is \'#{$first_name_txt}\'. "
+        button "change" do
+          $first_name_txt = ask("What\'s your first name, for realz?")
+          @conf_first.text = "* your first name is \'#{$first_name_txt}\'. "
+        end
+      end
+      flow do
+        @conf_last = para "* your last name is \'#{$last_name_txt}\'. "
+        button "change" do
+          $last_name_txt = ask("What\'s your last name, for realz?")
+          @conf_last.text = para "* your last name is \'#{$last_name_txt}\'. "
+        end
+      end
+      flow do
+        @conf_num = para "* your C-number or DL number is \'#{$c_number_txt}\'. "
+        button "change" do
+          $c_number_txt = ask("What\'s your C-number or DL number, for realz?")
+          @conf_num.text = para "* your C-number or DL number is \'#{$c_number_txt}\'. "
+        end
+      end
+      flow do
+        @conf_sex = para "* you\'re a: "
+        @check_m_review = check do
+          @check_f_review.checked = false
+          $sex = true
+        end
+        para "male | "
+        @check_f_review = check do
+          @check_m_review.checked = false
+          $sex = false
+        end
+        para "female"
+        if $sex
+          @check_m_review.checked = true
+          @check_f_review.checked = false
+        else
+          @check_f_review.checked = true
+          @check_m_review.checked = false
+        end
+      end
+    end
+    stack :margin_left => 240, :margin => 10, :margin_top => 75 do
+      button "submit" do
+        submit_registration($first_name_txt, $last_name_txt, $c_number_txt, $sex)
+        visit "/success"
+      end
+    end
+  end
+
   def success
+    background white
+    stack :margin => 10, :margin_left => 150, :margin_top => 20 do
+      banner "Success!", :margin => 4
+    end
+    stack :margin => 75, :margin_top => 20 do
+      subtitle "Successfully registered, thanks " + $first_name_txt + "!", :align => "center"
+      timer 5 do
+        visit "/"
+      end
+      para link("go back", :click => "/"), :align => "center"
+      inscription "(this page will go back to registration in 5 seconds)", :align => "center"
+    end
+  end
+
+  def first_name
+    background white
+    stack :margin => 10, :margin_left => 150, :margin_top => 20 do
+      banner "First Name"
+    end
+    stack :margin => 10, :margin_left => 130 do
+      subtitle "Whats yo\' name shawty?"
+    end
     stack :margin_left => 130, :margin => 10, :margin_top => 20 do
-      banner "Successfully registered, thanks " + $first_name.text
-      para link("go back", :click => "/")
+      tagline "First name, or at least something you\'ll answer to?"
+      $first_name = edit_line :width => 300
+      if $first_name_txt != ""
+        para "You entered \'#{$first_name_txt}\' last time, just fyi."
+      end
+    end
+    stack :margin_left => 240, :margin => 10, :margin_top => 75 do
+      flow :margin => 20 do
+        button "back" do
+          if confirm("If you go back from here you\'ll lose the info you\'ve entered. Are you sure?")
+            visit "/"
+          end
+        end
+        button "next" do
+          ready = true
+          if $first_name_txt == ""
+            if $first_name.text == "" or $first_name.text.length < 2
+              alert "Please enter a first name of at least 2 characters."
+              ready = false
+            end
+            if $first_name.text == "  "
+              alert "Please enter something other than spaces for a first name."
+              ready = false
+            end
+          end
+          if ready
+            $first_name_txt = $first_name.text
+            visit "/last_name"
+          end
+        end
+      end
+    end
+  end
+
+  def last_name
+    background white
+    stack :margin => 10, :margin_left => 150, :margin_top => 20 do
+      banner "Last Name"
+      subtitle "From which clan doth thou hail?"
+    end
+    stack :margin_left => 130, :margin => 10, :margin_top => 20 do
+      tagline "Last name, \'cause what if there\'s another #{$first_name.text}?"
+      $last_name = edit_line :width => 300
+      if $last_name_txt != ""
+        para "You entered \'#{$last_name_txt}\' last time, just fyi."
+      end
+    end
+    stack :margin_left => 240, :margin => 10, :margin_top => 75 do
+      flow :margin => 20 do
+        button "back" do
+          visit "/first_name"
+        end
+        button "next" do
+          ready = true
+          if $last_name_txt == ""
+            if $last_name.text == "" or $last_name.text.length < 2
+              alert "Please enter a last name of at least 2 characters."
+              ready = false
+            end
+            if $last_name.text == "  "
+              alert "Please enter something other than spaces for a last name."
+              ready = false
+            end
+          end
+          if ready
+            $last_name_txt = $last_name.text
+            visit "/cnumber"
+          end
+        end
+      end
+    end
+  end
+
+  def cnumber
+    background white
+    stack :margin => 10, :margin_left => 150, :margin_top => 20 do
+      banner "C Number"
+      subtitle "Lemme get yo digits, son."
+    end
+    stack :margin_left => 130, :margin => 10, :margin_top => 20 do
+      tagline "C number, or if you don't have one, your DL number."
+      $c_number = edit_line :width => 300
+      if $c_number_txt != ""
+        para "(You entered \'#{$c_number_txt}\' last time, just fyi.)"
+      end
+    end
+    stack :margin_left => 240, :margin => 10, :margin_top => 75 do
+      flow :margin => 20 do
+        button "back" do
+          visit "/last_name"
+        end
+        button "next" do
+          ready = true
+          if $c_number_txt == ""
+            if $c_number.text.length < 8 or $c_number.text.length > 10
+              alert "Please enter a valid C-number of DL number."
+              ready = false
+            end
+          end
+          if ready
+            $c_number_txt = $c_number.text
+            visit "/sex_choose"
+          end
+        end
+      end
+    end
+  end
+
+  def sex_choose
+    background white
+    stack :margin => 10, :margin_left => 120, :margin_top => 20 do
+      banner "Pick your Sex"
+    end
+    stack :margin => 10, :margin_left => 30 do
+      @my_sub = tagline "You can't be both (without proof),\n so choose wisely."
+      @my_sub.align = "center"
+    end
+    stack :margin_left => 185, :margin => 10, :margin_top => 20 do
+      flow do
+        image("../imgs/male.png") do
+          @male_check = true
+          @female_check = false
+          @sex_text.text = "You\'re a boy, don't dry hump the girls."
+        end
+        subtitle " or "
+        image("../imgs/female.png") do
+          @female_check = true
+          @male_check = false
+          @sex_text.text = "You\'re a girl, make sure you have pepper spray."
+        end
+      end
+    end
+    stack :margin_left => 30, :margin => 10, :margin_top => 30 do
+      @sex_text = caption "You're asexual. (click your sex)"
+      @sex_text.align = "center"
+    end
+    stack :margin_left => 220, :margin => 10, :margin_top => 30 do
+      flow :margin => 20 do
+        button "back" do
+          visit "/cnumber"
+        end
+        button "next" do
+          ready = true
+          if not @male_check and not @female_check
+            alert "No asexual beings permitted without proof, pick a sex."
+            ready = false
+          end
+          if ready
+            $sex = if @male_check then true else false end
+            visit "/review"
+          end
+        end
+      end
     end
   end
 
   def register
+    $c_number_txt = ""; $first_name_txt = ""; $last_name_txt = "";
+    background white
     stack :margin => 10, :margin_left => 150, :margin_top => 20 do
-      banner "Registration", :margin => 4
+      banner "Welcome"
     end
-    stack :margin_left => 130, :margin => 10, :margin_top => 20 do
-      para "lets register nigga"
-      flow do
-        para "First Name: "
-        $first_name = edit_line :width => 300
-      end
-      flow do
-        para "Last Name:  "
-        @last_name = edit_line :width => 300
-      end
-      flow do
-        para "C Number:   "
-        @c_number = edit_line :width => 300
-      end
-      flow do
-        para "Male ";
-        @male_check = check do
-          @female_check.checked = false if @male_check.checked?
-        end
-        para "  or  Female ";
-        @female_check = check do
-          @male_check.checked = false if @female_check.checked?
-        end
-      end
-      @b1 = button "register" do
-        ready = true
-        if $first_name.text.length < 2 or $first_name.text == ""
-          alert "Please enter a first name of at least 2 characters."
-          ready = false
+    stack :margin_left => 90, :width => 550, :margin => 10, margin_top => 20 do
+      caption "So here's our guidelines/disclaimer and stuff:"
+      inscription "1. If you break it, you buy it, in its most updated/expensive version. This isn't up for discussion. Items covered under this clause include but are not limited to: any game console, TV, computer, network router, controller, speaker system or other peripheral involved in this tournament. We reserve the right to hold you to the price of anything else you may break that is not your property."
+      inscription "2. You're responsible for your things. If you lose something, thats your problem not the UM IEEE's."
+      inscription "3. Exercise self control and respect. If you can't keep your hands to yourself or you can't say anything nice, we'll just ask you to leave. And by ask, we mean you will leave."
+      inscription "4. If you have a problem with someone, take it up with a referee, let him beat the snot out of the other kid... erm I mean handle the problem."
+      inscription "5. All match outcomes are final. No do-overs unless the referee deems a rematch necessary due to hardware problems, heart attacks or acts of God/Allah/Yahweh/$DEITY."
+    end
+    flow :margin_left => 100, :width => 500, :margin => 10, :margin_top => 20 do
+      @agree = check;
+      inscription "I agree to these guidelines and any others that may spontaneously arise during the duration of this event. (Though we will try not to make absurd rules.)"
+    end
+    stack :margin_left => 240, :margin => 10, :margin_top => 10 do
+      button "next" do
+        if @agree.checked?
+          visit "/first_name"
         else
-        end
-        if $first_name.text == "  "
-          alert "Please enter something other than spaces for a first name."
-          ready = false
-        end
-        if @last_name.text.length < 2 or @last_name.text == ""
-          alert "Please enter a last name of at least 2 characters."
-          ready = false
-        end
-        if @last_name.text == "  "
-          alert "Please enter something other than spaces for a last name."
-          ready = false
-        end
-        if @c_number.text.length < 8
-          alert "Please enter a valid C number."
-          ready = false
-        end
-        if not @male_check.checked? and not @female_check.checked?
-          alert "No asexual beings permitted without proof, pick a sex."
-          ready = false
-        end
-
-        if ready
-          sex = if @male_check.checked? then true else false end
-          submit_registration($first_name, @last_name, @c_number, sex)
-          #alert "Successfully registered, thanks."
-          visit "/success"
+          alert "You'll need to agree to this before you can register, srsly, kthxbai!"
         end
       end
     end
   end
 end
 
-Shoes.app :width => 640, :height => 450,
+Shoes.app :width => 640, :height => 700,
 :title => "Tourny Manager. Ya, we doin' it real big son."
